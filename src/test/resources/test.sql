1669105b62b8ca0c176d56162604849bcce774b5
anaboru <slysetrrr71969@gmail.com>
2022-11-09 20:35:35 +0300

    50

--- src/test/resources/test.sql
+++
@@ -1,5 +0,0 @@
-insert into users (name, age, email) values('Steve', 55, 'steve.gorbunov@gmail.com');
-#select * from users;
-#select id, email, age from users where name = 'Steve';
-#update users set name = 'Steve', age = 55 where id = 1;
-#delete from users where id = 4;
