ace672112ab8580c1f55bf243a5acd44df2ffdda
anaboru <slysetrrr71969@gmail.com>
2022-11-09 09:43:02 +0300

    26

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        UserServiceImpl usi = new UserServiceImpl();
+        usi.createUsersTable();
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("Иван", "Иванов", (byte) 11));
+        userList.add(new User("Роман", "Романов", (byte) 22));
+        userList.add(new User("Игорь", "Игорев", (byte) 33));
+        userList.add(new User("Пётр", "Петров", (byte) 44));
+        userList.forEach(us -> usi.saveUser(us.getName(), us.getLastName(), us.getAge()));
+        System.out.println(usi.getAllUsers());
+        usi.cleanUsersTable();
+        usi.dropUsersTable();
     }
 }
